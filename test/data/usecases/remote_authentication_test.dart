5fd36c70833918cc8b47f4df4d6a4d533030bb57
rubem-pontomais <rubem.vasconcelos@pontomais.com.br>
2022-01-31 20:13:46 -0300

    feature: ensure RemoteAuthentication throws UnexpectedError on 404

--- test/data/usecases/remote_authentication_test.dart
+++ test/data/usecases/remote_authentication_test.dart
@@ -43,4 +43,16 @@ void main() {
 
     expect(future, throwsA(DomainError.unexpected));
   });
+
+  test('Should throw UnexpectedError if HpptClient returns 404', () async {
+    when(httpClient.request(
+      url: anyNamed('url'),
+      method: anyNamed('method'),
+      body: anyNamed('body'),
+    )).thenThrow(HttpError.notFound);
+
+    final future = sut.auth(params);
+
+    expect(future, throwsA(DomainError.unexpected));
+  });
 }
