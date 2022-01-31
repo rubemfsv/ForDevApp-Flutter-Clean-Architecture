85443dafdb0bac1fcc084b742dc25bf5588cf489
rubem-pontomais <rubem.vasconcelos@pontomais.com.br>
2022-01-31 20:18:30 -0300

    feature: ensure RemoteAuthentication throws InvalidCredentialsError on 401

--- test/data/usecases/remote_authentication_test.dart
+++ test/data/usecases/remote_authentication_test.dart
@@ -44,6 +44,17 @@ void main() {
     expect(future, throwsA(DomainError.unexpected));
   });
 
+  test('Should throw InvalidCredentialsError if HpptClient returns 401', () async {
+    when(httpClient.request(
+      url: anyNamed('url'),
+      method: anyNamed('method'),
+      body: anyNamed('body'),
+    )).thenThrow(HttpError.unauthorized);
+
+    final future = sut.auth(params);
+
+    expect(future, throwsA(DomainError.invalidCredentials));
+  });
   test('Should throw UnexpectedError if HpptClient returns 404', () async {
     when(httpClient.request(
       url: anyNamed('url'),
