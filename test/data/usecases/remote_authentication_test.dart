d7ffe693a03f6c9bb726e20fb9f89e09bba531c3
rubem-pontomais <rubem.vasconcelos@pontomais.com.br>
2022-02-01 18:22:04 -0300

    chore: ensure RemoteAuthentication throws unexpectedError on 200 with invalid data

--- test/data/usecases/remote_authentication_test.dart
+++ test/data/usecases/remote_authentication_test.dart
@@ -53,6 +53,20 @@ void main() {
     expect(account.token, accessToken);
   });
 
+  test(
+      'Should throw UnexpectedError if HpptClient returns 200 with invalid data',
+      () async {
+    when(httpClient.request(
+      url: anyNamed('url'),
+      method: anyNamed('method'),
+      body: anyNamed('body'),
+    )).thenAnswer((_) async => {'invalid_key': 'invalid_value'});
+
+    final future = sut.auth(params);
+
+    expect(future, throwsA(DomainError.unexpected));
+  });
+
   test('Should throw UnexpectedError if HpptClient returns 400', () async {
     when(httpClient.request(
       url: anyNamed('url'),
