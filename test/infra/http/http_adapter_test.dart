658bb0ac62a9e27da194e2c050823713e96351b9
rubem-pontomais <rubem.vasconcelos@pontomais.com.br>
2022-02-01 19:02:43 -0300

    feature: ensure HttpAdapter calls post with correct headers

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -10,7 +10,11 @@ class HttpAdapter {
   HttpAdapter(this.client);
 
   Future<void> request({@required String url, @required String method}) async {
-    await client.post(url);
+    final headers = {
+      'content-type': 'application/json',
+      'accept': 'application/json'
+    };
+    await client.post(url, headers: headers);
   }
 }
 
@@ -25,7 +29,10 @@ void main() {
 
       await sut.request(url: url, method: 'post');
 
-      verify(client.post(url));
+      verify(client.post(url, headers: {
+        'content-type': 'application/json',
+        'accept': 'application/json'
+      }));
     });
   });
 }
