030951a42d3e53a4ae9c0bc2fbf2e924402b30d0
rubem-pontomais <rubem.vasconcelos@pontomais.com.br>
2022-02-01 19:07:18 -0300

    feature: ensure HttpAdapter calls post with correct body

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -1,3 +1,4 @@
+import 'dart:convert';
 import 'package:faker/faker.dart';
 import 'package:http/http.dart';
 import 'package:meta/meta.dart';
@@ -9,12 +10,13 @@ class HttpAdapter {
 
   HttpAdapter(this.client);
 
-  Future<void> request({@required String url, @required String method}) async {
+  Future<void> request(
+      {@required String url, @required String method, Map body}) async {
     final headers = {
       'content-type': 'application/json',
       'accept': 'application/json'
     };
-    await client.post(url, headers: headers);
+    await client.post(url, headers: headers, body: jsonEncode(body));
   }
 }
 
@@ -32,12 +34,15 @@ void main() {
       url = faker.internet.httpUrl();
     });
     test('Should call post with correct values', () async {
-      await sut.request(url: url, method: 'post');
+      await sut
+          .request(url: url, method: 'post', body: {'any_key': 'any_value'});
 
-      verify(client.post(url, headers: {
-        'content-type': 'application/json',
-        'accept': 'application/json'
-      }));
+      verify(client.post(url,
+          headers: {
+            'content-type': 'application/json',
+            'accept': 'application/json'
+          },
+          body: '{"any_key":"any_value"}'));
     });
   });
 }
