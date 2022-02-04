cf016f6b10427c01acadcf2ff47c4cd91850f1ba
rubem-pontomais <rubem.vasconcelos@pontomais.com.br>
2022-02-04 16:30:58 -0300

    feature: ensure HttpAdapter throws BadRequestError on 400

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -3,6 +3,8 @@ import 'package:http/http.dart';
 import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
 
+import 'package:hear_mobile/data/http/http.dart';
+
 import 'package:hear_mobile/infra/http/http.dart';
 
 class ClientSpy extends Mock implements Client {}
@@ -78,6 +80,14 @@ void main() {
       final response = await sut.request(url: url, method: 'post');
 
       expect(response, null);
+    });
+
+    test('Should return BadRequestError if post returns 400', () async {
+      mockResponse(400);
+
+      final future = sut.request(url: url, method: 'post');
+
+      expect(future, throwsA(HttpError.badRequest));
     });
   });
 }
