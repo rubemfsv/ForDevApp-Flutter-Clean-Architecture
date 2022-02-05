f4757534182bae2442908edeec376a8ef0d9f100
rubem-pontomais <rubem.vasconcelos@pontomais.com.br>
2022-02-05 18:21:07 -0300

    feature: ensure HttpAdapter throws serverError if post throws

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -35,6 +35,8 @@ void main() {
             {String body = '{"any_key":"any_value"}'}) =>
         mockRequest().thenAnswer((_) async => Response(body, statusCode));
 
+    void mockError() => mockRequest().thenThrow(Exception());
+
     setUp(() {
       mockResponse(200);
     });
@@ -133,6 +135,14 @@ void main() {
     test('Should return ServerError if post returns 500', () async {
       mockResponse(500);
 
+      final future = sut.request(url: url, method: 'post');
+
+      expect(future, throwsA(HttpError.serverError));
+    });
+
+    test('Should return ServerError if post throws', () async {
+      mockError();
+      
       final future = sut.request(url: url, method: 'post');
 
       expect(future, throwsA(HttpError.serverError));
