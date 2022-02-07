f5b42e3d9c9af7107a01b234c7a377a3b871b390
rubem-pontomais <rubem.vasconcelos@pontomais.com.br>
2022-02-07 19:20:50 -0300

    chore: change statusbar to light mode

--- lib/ui/components/app.dart
+++ lib/ui/components/app.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import '../pages/pages.dart';
 
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.light);
+
     final primaryColor = Color.fromRGBO(136, 14, 79, 1);
     final primaryColorDark = Color.fromRGBO(96, 0, 39, 1);
     final primaryColorLight = Color.fromRGBO(188, 71, 123, 1);
@@ -27,11 +30,9 @@ class App extends StatelessWidget {
         ),
         inputDecorationTheme: InputDecorationTheme(
           enabledBorder: UnderlineInputBorder(
-            borderSide: BorderSide(color: primaryColorLight)
-          ),
-          focusedBorder: UnderlineInputBorder(
-            borderSide: BorderSide(color: primaryColor)
-          ),
+              borderSide: BorderSide(color: primaryColorLight)),
+          focusedBorder:
+              UnderlineInputBorder(borderSide: BorderSide(color: primaryColor)),
           alignLabelWithHint: true,
         ),
         buttonTheme: ButtonThemeData(
@@ -41,7 +42,7 @@ class App extends StatelessWidget {
           padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
           textTheme: ButtonTextTheme.primary,
           shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(20), 
+            borderRadius: BorderRadius.circular(20),
           ),
         ),
       ),
