be2143644646c5c7e1053dbf928b3c44b29c1353
rubem-pontomais <rubem.vasconcelos@pontomais.com.br>
2022-02-14 08:14:26 -0300

    chore: move page initialization to a function helper

--- test/ui/pages/login_page_test.dart
+++ test/ui/pages/login_page_test.dart
@@ -4,10 +4,14 @@ import 'package:flutter/material.dart';
 import 'package:hear_mobile/ui/pages/pages.dart';
 
 void main() {
-  testWidgets("Should load with correct initial state",
-      (WidgetTester tester) async {
+  Future<void> loadPage(WidgetTester tester) async {
     final loginPage = MaterialApp(home: LoginPage());
     await tester.pumpWidget(loginPage);
+  }
+
+  testWidgets("Should load with correct initial state",
+      (WidgetTester tester) async {
+    await loadPage(tester);
 
     final emailTextChildren = find.descendant(
         of: find.bySemanticsLabel('Email'), matching: find.byType(Text));
@@ -27,4 +31,5 @@ void main() {
 
     expect(button.onPressed, null);
   });
+
 }
