798e8de04dea65fc4cddf1bbeefe1a2bdffd1841
rubem-pontomais <rubem.vasconcelos@pontomais.com.br>
2022-02-14 08:39:50 -0300

    feature: ensure loginPage shows no error if email is valid

--- lib/ui/pages/login/login_page.dart
+++ lib/ui/pages/login/login_page.dart
@@ -32,7 +32,7 @@ class LoginPage extends StatelessWidget {
                             Icons.email,
                             color: Theme.of(context).primaryColorLight,
                           ),
-                          errorText: snapshot.data,
+                          errorText: snapshot.data?.isEmpty == true ? null : snapshot.data,
                         ),
                         keyboardType: TextInputType.emailAddress,
                         onChanged: presenter.validateEmail,
