a93ac08aeaf29ab0347a85f3966346368a6c07e4
rubem-pontomais <rubem.vasconcelos@pontomais.com.br>
2022-02-14 08:55:33 -0300

    feature: ensure loginPage enables button if form is valid

--- lib/ui/pages/login/login_page.dart
+++ lib/ui/pages/login/login_page.dart
@@ -23,44 +23,52 @@ class LoginPage extends StatelessWidget {
                   child: Column(
                 children: <Widget>[
                   StreamBuilder<String>(
-                    stream: presenter.emailErrorStream,
-                    builder: (context, snapshot) {
-                      return TextFormField(
-                        decoration: InputDecoration(
-                          labelText: 'Email',
-                          icon: Icon(
-                            Icons.email,
-                            color: Theme.of(context).primaryColorLight,
-                          ),
-                          errorText: snapshot.data?.isEmpty == true ? null : snapshot.data,
-                        ),
-                        keyboardType: TextInputType.emailAddress,
-                        onChanged: presenter.validateEmail,
-                      );
-                    }
-                  ),
-                  Padding(
-                    padding: EdgeInsets.only(top: 8, bottom: 32),
-                    child: StreamBuilder<String>(
-                      stream: presenter.passwordErrorStream,
+                      stream: presenter.emailErrorStream,
                       builder: (context, snapshot) {
                         return TextFormField(
                           decoration: InputDecoration(
-                            labelText: 'Senha',
+                            labelText: 'Email',
                             icon: Icon(
-                              Icons.lock,
+                              Icons.email,
                               color: Theme.of(context).primaryColorLight,
                             ),
-                            errorText: snapshot.data?.isEmpty == true ? null : snapshot.data,
+                            errorText: snapshot.data?.isEmpty == true
+                                ? null
+                                : snapshot.data,
                           ),
-                          obscureText: true,
-                          onChanged: presenter.validatePassword,
+                          keyboardType: TextInputType.emailAddress,
+                          onChanged: presenter.validateEmail,
                         );
-                      }
-                    ),
+                      }),
+                  Padding(
+                    padding: EdgeInsets.only(top: 8, bottom: 32),
+                    child: StreamBuilder<String>(
+                        stream: presenter.passwordErrorStream,
+                        builder: (context, snapshot) {
+                          return TextFormField(
+                            decoration: InputDecoration(
+                              labelText: 'Senha',
+                              icon: Icon(
+                                Icons.lock,
+                                color: Theme.of(context).primaryColorLight,
+                              ),
+                              errorText: snapshot.data?.isEmpty == true
+                                  ? null
+                                  : snapshot.data,
+                            ),
+                            obscureText: true,
+                            onChanged: presenter.validatePassword,
+                          );
+                        }),
                   ),
-                  RaisedButton(
-                      onPressed: null, child: Text('Entrar'.toUpperCase())),
+                  StreamBuilder<bool>(
+                      stream: presenter.isFormValidStreamStream,
+                      builder: (context, snapshot) {
+                        return RaisedButton(
+                          onPressed: snapshot.data == true ? () {} : null,
+                          child: Text('Entrar'.toUpperCase()),
+                        );
+                      }),
                   FlatButton.icon(
                       onPressed: () {},
                       icon: Icon(Icons.person),
--- lib/ui/pages/login/login_presenter.dart
+++ lib/ui/pages/login/login_presenter.dart
@@ -1,6 +1,7 @@
 abstract class LoginPresenter {
   Stream get emailErrorStream;
   Stream get passwordErrorStream;
+  Stream get isFormValidStreamStream;
 
   void validateEmail(String email);
   void validatePassword(String password);
