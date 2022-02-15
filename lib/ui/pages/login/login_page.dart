ec85fb7d14b78ecb42c1e2492a100696840eb283
rubem-pontomais <rubem.vasconcelos@pontomais.com.br>
2022-02-15 09:00:39 -0300

    feature: ensure loginpage closes streams on dispose

--- lib/ui/pages/login/login_page.dart
+++ lib/ui/pages/login/login_page.dart
@@ -1,19 +1,30 @@
 import 'package:flutter/material.dart';
 
-import 'package:hear_mobile/ui/components/components.dart';
 import './login_presenter.dart';
+import 'package:hear_mobile/ui/components/components.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   final LoginPresenter presenter;
 
   LoginPage(this.presenter);
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  void dispose() {
+    super.dispose();
+    widget.presenter.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Builder(
         builder: (context) {
-          presenter.isLoadingStream.listen((isLoading) {
+          widget.presenter.isLoadingStream.listen((isLoading) {
             if (isLoading) {
               showDialog(
                 context: context,
@@ -38,7 +49,7 @@ class LoginPage extends StatelessWidget {
             }
           });
 
-          presenter.mainErrorStream.listen((error) {
+          widget.presenter.mainErrorStream.listen((error) {
             if (error != null) {
               Scaffold.of(context).showSnackBar(
                 SnackBar(
@@ -61,7 +72,7 @@ class LoginPage extends StatelessWidget {
                     child: Column(
                       children: <Widget>[
                         StreamBuilder<String>(
-                            stream: presenter.emailErrorStream,
+                            stream: widget.presenter.emailErrorStream,
                             builder: (context, snapshot) {
                               return TextFormField(
                                 decoration: InputDecoration(
@@ -75,13 +86,13 @@ class LoginPage extends StatelessWidget {
                                       : snapshot.data,
                                 ),
                                 keyboardType: TextInputType.emailAddress,
-                                onChanged: presenter.validateEmail,
+                                onChanged: widget.presenter.validateEmail,
                               );
                             }),
                         Padding(
                           padding: EdgeInsets.only(top: 8, bottom: 32),
                           child: StreamBuilder<String>(
-                              stream: presenter.passwordErrorStream,
+                              stream: widget.presenter.passwordErrorStream,
                               builder: (context, snapshot) {
                                 return TextFormField(
                                   decoration: InputDecoration(
@@ -96,16 +107,16 @@ class LoginPage extends StatelessWidget {
                                         : snapshot.data,
                                   ),
                                   obscureText: true,
-                                  onChanged: presenter.validatePassword,
+                                  onChanged: widget.presenter.validatePassword,
                                 );
                               }),
                         ),
                         StreamBuilder<bool>(
-                            stream: presenter.isFormValidStream,
+                            stream: widget.presenter.isFormValidStream,
                             builder: (context, snapshot) {
                               return RaisedButton(
                                 onPressed: snapshot.data == true
-                                    ? presenter.auth
+                                    ? widget.presenter.auth
                                     : null,
                                 child: Text('Entrar'.toUpperCase()),
                               );
