f260d8ba2d6d3fc9c08fc278186c393d6876af45
rubem-pontomais <rubem.vasconcelos@pontomais.com.br>
2022-02-15 08:33:52 -0300

    feature: ensure loginpage presents loading

--- lib/ui/pages/login/login_presenter.dart
+++ lib/ui/pages/login/login_presenter.dart
@@ -1,7 +1,8 @@
 abstract class LoginPresenter {
   Stream get emailErrorStream;
   Stream get passwordErrorStream;
-  Stream get isFormValidStreamStream;
+  Stream get isFormValidStream;
+  Stream get isLoadingStream;
 
   void validateEmail(String email);
   void validatePassword(String password);
