8d5a46bd1451965b06f3ab3a44276a9f0745719e
rubem-pontomais <rubem.vasconcelos@pontomais.com.br>
2022-02-15 08:21:49 -0300

    feature: ensure loginpage calls authentication on form submit

--- lib/ui/pages/login/login_page.dart
+++ lib/ui/pages/login/login_page.dart
@@ -65,7 +65,7 @@ class LoginPage extends StatelessWidget {
                       stream: presenter.isFormValidStreamStream,
                       builder: (context, snapshot) {
                         return RaisedButton(
-                          onPressed: snapshot.data == true ? () {} : null,
+                          onPressed: snapshot.data == true ? presenter.auth : null,
                           child: Text('Entrar'.toUpperCase()),
                         );
                       }),
