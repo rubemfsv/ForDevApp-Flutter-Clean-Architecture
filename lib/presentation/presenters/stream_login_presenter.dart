dcc9deefb4ce332b81abd76cee8f8fcc39802007
rubem-pontomais <rubem.vasconcelos@pontomais.com.br>
2022-02-16 08:35:41 -0300

    chore: moves StreamLoginPresenter to its own file

---
+++ lib/presentation/presenters/stream_login_presenter.dart
@@ -0,0 +1,23 @@
+import 'dart:async';
+import '../protocols/protocols.dart';
+import 'package:meta/meta.dart';
+
+class LoginState {
+  String emailError;
+}
+
+class StreamLoginPresenter {
+  final Validation validation;
+  final _controller = StreamController<LoginState>.broadcast();
+  final _state = LoginState();
+
+  Stream<String> get emailErrorStream =>
+      _controller.stream.map((state) => state.emailError);
+
+  StreamLoginPresenter({@required this.validation});
+
+  void validateEmail(String email) {
+    _state.emailError = validation.validate(field: 'email', value: email);
+    _controller.add(_state);
+  }
+}
