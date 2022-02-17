cf4687dece15ed429f635068c0f83f40786a00de
rubem-pontomais <rubem.vasconcelos@pontomais.com.br>
2022-02-17 07:32:02 -0300

    chore: ensure formValid emits false if email is false

--- lib/presentation/presenters/stream_login_presenter.dart
+++ lib/presentation/presenters/stream_login_presenter.dart
@@ -4,6 +4,7 @@ import 'package:meta/meta.dart';
 
 class LoginState {
   String emailError;
+  bool get isFormValid => false;
 }
 
 class StreamLoginPresenter {
@@ -13,6 +14,8 @@ class StreamLoginPresenter {
 
   Stream<String> get emailErrorStream =>
       _controller.stream.map((state) => state.emailError).distinct();
+  Stream<bool> get isFormValidStream =>
+      _controller.stream.map((state) => state.isFormValid).distinct();
 
   StreamLoginPresenter({@required this.validation});
 
