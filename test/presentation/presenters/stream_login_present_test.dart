a5b7f4db9ff1ab2709d7a75935ac82b21602ed62
rubem-pontomais <rubem.vasconcelos@pontomais.com.br>
2022-02-17 07:35:41 -0300

    feature: ensure StreamLoginPresenter calls validation on password changed

--- test/presentation/presenters/stream_login_present_test.dart
+++ test/presentation/presenters/stream_login_present_test.dart
@@ -11,6 +11,7 @@ void main() {
   StreamLoginPresenter sut;
   ValidationSpy validation;
   String email;
+  String password;
 
   PostExpectation mockValidationCall(String field) => when(validation.validate(
         field: field == null ? anyNamed('field') : field,
@@ -25,6 +26,7 @@ void main() {
     validation = ValidationSpy();
     sut = StreamLoginPresenter(validation: validation);
     email = faker.internet.email();
+    password = faker.internet.password();
     mockValidation();
   });
 
@@ -54,5 +56,11 @@ void main() {
 
     sut.validateEmail(email);
     sut.validateEmail(email);
+  });
+
+   test('Should call Validation with correct password', () {
+    sut.validatePassword(password);
+
+    verify(validation.validate(field: 'password', value: password)).called(1);
   });
 }
