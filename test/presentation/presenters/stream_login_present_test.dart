f2cdf092b830b0c91cd882a542a68cec6f535dad
rubem-pontomais <rubem.vasconcelos@pontomais.com.br>
2022-02-17 08:54:50 -0300

    feature: ensure StreamLoginPresenter emits correct events on UnexpectedError

--- test/presentation/presenters/stream_login_present_test.dart
+++ test/presentation/presenters/stream_login_present_test.dart
@@ -161,4 +161,16 @@ void main() {
 
     await sut.auth();
   });
+
+  test('Should emit correct events on unexpectedError', () async {
+    mockAuthenticationError(DomainError.unexpected);
+    sut.validateEmail(email);
+    sut.validatePassword(password);
+
+    expectLater(sut.isLoadingStream, emits(false));
+    sut.mainErrorStream.listen(
+        expectAsync1((error) => expect(error, 'Algo errado aconteceu. Tente novamente em breve.')));
+
+    await sut.auth();
+  });
 }
