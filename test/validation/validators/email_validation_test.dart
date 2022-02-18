71a3283e0bde577c1433a5606d17cad66b87fe2c
rubem-pontomais <rubem.vasconcelos@pontomais.com.br>
2022-02-18 18:27:52 -0300

    feature: ensure emailValidation returns null if email is empty or null

---
+++ test/validation/validators/email_validation_test.dart
@@ -0,0 +1,29 @@
+import 'package:test/test.dart';
+import 'package:hear_mobile/validation/protocols/field_validation.dart';
+import 'package:hear_mobile/validation/validators/validators.dart';
+
+class EmailValidation implements FieldValidation {
+  final String field;
+
+  EmailValidation(this.field);
+
+  String validate(String value) {
+    return null;
+  }
+}
+
+void main() {
+  EmailValidation sut;
+
+  setUp(() {
+    sut = EmailValidation('any_field');
+  });
+
+  test('Should return null if email is empty', () {
+    expect(sut.validate(''), null);
+  });
+
+  test('Should return null if email is null', () {
+    expect(sut.validate(null), null);
+  });
+}
