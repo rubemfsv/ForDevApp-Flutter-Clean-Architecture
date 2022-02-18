8acdb235ac1734ec9f4e52f6968e02171b1f5c80
rubem-pontomais <rubem.vasconcelos@pontomais.com.br>
2022-02-18 18:34:58 -0300

    feature: ensure emailValidation returns error if email is invalid

--- test/validation/validators/email_validation_test.dart
+++ test/validation/validators/email_validation_test.dart
@@ -3,12 +3,16 @@ import 'package:hear_mobile/validation/protocols/field_validation.dart';
 import 'package:hear_mobile/validation/validators/validators.dart';
 
 class EmailValidation implements FieldValidation {
+  final regex = RegExp(
+      r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+");
   final String field;
 
   EmailValidation(this.field);
 
   String validate(String value) {
-    return null;
+    final isValid = value?.isNotEmpty != true || regex.hasMatch(value);
+
+    return isValid ? null : 'Campo inválido';
   }
 }
 
@@ -29,5 +33,9 @@ void main() {
 
   test('Should return null if email is valid', () {
     expect(sut.validate('rfsv@cesar.school'), null);
+  });
+
+  test('Should return error if email is invalid', () {
+    expect(sut.validate('rfsv'), 'Campo inválido');
   });
 }
