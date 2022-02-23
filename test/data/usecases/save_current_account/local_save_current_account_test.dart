423469d40daae742a6fe79c3b871c0225fb35df0
rubem-pontomais <rubem.vasconcelos@pontomais.com.br>
2022-02-23 19:16:19 -0300

    chore: move production code to those own files

--- test/data/usecases/save_current_account/local_save_current_account_test.dart
+++ test/data/usecases/save_current_account/local_save_current_account_test.dart
@@ -1,32 +1,11 @@
 import 'package:faker/faker.dart';
-import 'package:meta/meta.dart';
 import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
 
 import 'package:hear_mobile/domain/helpers/helpers.dart';
 import 'package:hear_mobile/domain/entities/entities.dart';
-import 'package:hear_mobile/domain/usecases/usecases.dart';
-
-class LocalSaveCurrentAccount implements SaveCurrentAccount {
-  final SaveSecureCacheStorage saveSecureCacheStorage;
-
-  LocalSaveCurrentAccount({@required this.saveSecureCacheStorage});
-
-  Future<void> save(AccountEntity account) async {
-    try {
-      await saveSecureCacheStorage.saveSecure(
-        key: 'token',
-        value: account.token,
-      );
-    } catch (error) {
-      throw DomainError.unexpected;
-    }
-  }
-}
-
-abstract class SaveSecureCacheStorage {
-  Future<void> saveSecure({@required String key, @required String value});
-}
+import 'package:hear_mobile/data/cache/cache.dart';
+import 'package:hear_mobile/data/usecases/usecases.dart';
 
 class SaveSecureCacheStorageSpy extends Mock implements SaveSecureCacheStorage {
 }
@@ -58,7 +37,7 @@ void main() {
 
   test('Should throw unexpectedError if SaveSecureCacheStorage throws', () {
     mockError();
-    
+
     final future = sut.save(account);
 
     expect(future, throwsA(DomainError.unexpected));
