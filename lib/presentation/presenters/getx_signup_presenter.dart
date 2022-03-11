c0eacd39efef52efe53ff6ff21286a9bfe43e33a
rubem-pontomais <rubem.vasconcelos@pontomais.com.br>
2022-03-11 17:11:38 -0300

    feature: ensure GetxSignUpPresenter emits correct errors on email change

---
+++ lib/presentation/presenters/getx_signup_presenter.dart
@@ -0,0 +1,45 @@
+import 'dart:async';
+import 'package:get/state_manager.dart';
+import 'package:meta/meta.dart';
+
+import '../../ui/helpers/helpers.dart';
+import '../../ui/pages/login/login_presenter.dart';
+import '../../domain/helpers/domain_error.dart';
+import '../../domain/usecases/usecases.dart';
+import '../protocols/protocols.dart';
+
+class GetxSignUpPresenter extends GetxController {
+  final Validation validation;
+
+  var _emailError = Rx<UIError>();
+  var _isFormValid = false.obs;
+
+  Stream<UIError> get emailErrorStream => _emailError.stream;
+  Stream<bool> get isFormValidStream => _isFormValid.stream;
+
+  GetxSignUpPresenter({
+    @required this.validation,
+  });
+
+  void validateEmail(String email) {
+    _emailError.value = _validateField(field: 'email', value: email);
+    _validateForm();
+  }
+
+  UIError _validateField({String field, String value}) {
+    final error = validation.validate(field: field, value: value);
+
+    switch (error) {
+      case ValidationError.invalidField:
+        return UIError.invalidField;
+      case ValidationError.requiredField:
+        return UIError.requiredField;
+      default:
+        return null;
+    }
+  }
+
+  void _validateForm() {
+    _isFormValid.value = false;
+  }
+}
