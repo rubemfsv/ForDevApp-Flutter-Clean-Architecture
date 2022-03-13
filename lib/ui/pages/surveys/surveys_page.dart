90fb361aefe805e2e57fbd76ed96518128080de8
rubem-pontomais <rubem.vasconcelos@pontomais.com.br>
2022-03-13 18:15:41 -0300

    feature: ensure LoadSurveys calls load data on page load

--- lib/ui/pages/surveys/surveys_page.dart
+++ lib/ui/pages/surveys/surveys_page.dart
@@ -1,11 +1,17 @@
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
-import 'package:hear_mobile/ui/pages/surveys/components/survey_item.dart';
 import '../../helpers/i18n/i18n.dart';
+import './components/survey_item.dart';
+import './surveys_presenter.dart';
 
 class SurveysPage extends StatelessWidget {
+  final SurveysPresenter presenter;
+
+  SurveysPage(this.presenter);
+
   @override
   Widget build(BuildContext context) {
+    presenter.loadData();
     return Scaffold(
       appBar: AppBar(title: Text(R.translations.surveys)),
       body: Padding(
---
+++ test/ui/pages/surveys_page_test.dart
@@ -0,0 +1,29 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:get/get.dart';
+
+import 'package:mockito/mockito.dart';
+
+import 'package:hear_mobile/ui/pages/pages.dart';
+
+class SurveysPresenterSpy extends Mock implements SurveysPresenter {}
+
+void main() {
+  SurveysPresenterSpy presenter;
+
+  Future<void> loadPage(WidgetTester tester) async {
+    presenter = SurveysPresenterSpy();
+
+    final surveysPage = GetMaterialApp(
+      initialRoute: '/surveys',
+      getPages: [GetPage(name: '/surveys', page: () => SurveysPage(presenter))],
+    );
+    await tester.pumpWidget(surveysPage);
+  }
+
+  testWidgets('Should call LoadSurveys on page load',
+      (WidgetTester tester) async {
+    await loadPage(tester);
+
+    verify(presenter.loadData()).called(1);
+  });
+}
