058df267e3d6770979f46bf271c03af475b70fc0
rubem-pontomais <rubem.vasconcelos@pontomais.com.br>
2022-03-13 16:19:14 -0300

    feature: ensure RemoteLoadSurveys throws unexpected error if httpClient returns 404

--- test/data/usecases/load_surveys/remote_load_surveys_test.dart
+++ test/data/usecases/load_surveys/remote_load_surveys_test.dart
@@ -38,6 +38,10 @@ void main() {
     mockRequest().thenAnswer((_) async => data);
   }
 
+  void mockHttpError(HttpError error) {
+    mockRequest().thenThrow(error);
+  }
+
   setUp(() {
     url = faker.internet.httpUrl();
     httpClient = HttpClientSpy();
@@ -76,6 +80,14 @@ void main() {
     mockHttpData([
       {'invalid_key': 'invalid_value'}
     ]);
+
+    final future = sut.load();
+
+    expect(future, throwsA(DomainError.unexpected));
+  });
+
+  test('Should throw UnexpectedError if HpptClient returns 404', () async {
+    mockHttpError(HttpError.notFound);
 
     final future = sut.load();
 
