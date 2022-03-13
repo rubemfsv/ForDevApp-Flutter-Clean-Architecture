8b3a837fc7db5538e8ec7a3f84a5332f0afdc7d9
rubem-pontomais <rubem.vasconcelos@pontomais.com.br>
2022-03-13 18:01:53 -0300

    chore: remove unused tests

--- test/ui/pages/signup_page_test.dart
+++ test/ui/pages/signup_page_test.dart
@@ -82,60 +82,6 @@ void main() {
     closeStreams();
   });
 
-  testWidgets("Should load with correct initial state",
-      (WidgetTester tester) async {
-    await loadPage(tester);
-
-    final nameTextChildren = find.descendant(
-        of: find.bySemanticsLabel(R.translations.nameLabel),
-        matching: find.byType(Text));
-
-    expect(
-      nameTextChildren,
-      findsOneWidget,
-      reason:
-          'when a TextFormField has only one text child, means it has no error, since one of the children is always the label text',
-    );
-
-    final emailTextChildren = find.descendant(
-        of: find.bySemanticsLabel(R.translations.emailLabel),
-        matching: find.byType(Text));
-
-    expect(
-      emailTextChildren,
-      findsOneWidget,
-      reason:
-          'when a TextFormField has only one text child, means it has no error, since one of the children is always the label text',
-    );
-
-    final passwordTextChildren = find.descendant(
-        of: find.bySemanticsLabel(R.translations.passwordLabel),
-        matching: find.byType(Text));
-
-    expect(
-      passwordTextChildren,
-      findsOneWidget,
-      reason:
-          'when a TextFormField has only one text child, means it has no error, since one of the children is always the label text',
-    );
-
-    final passwordConfirmationTextChildren = find.descendant(
-        of: find.bySemanticsLabel(R.translations.passwordConfirmationLabel),
-        matching: find.byType(Text));
-
-    expect(
-      passwordConfirmationTextChildren,
-      findsOneWidget,
-      reason:
-          'when a TextFormField has only one text child, means it has no error, since one of the children is always the label text',
-    );
-
-    final button = tester.widget<RaisedButton>(find.byType(RaisedButton));
-
-    expect(button.onPressed, null);
-    expect(find.byType(CircularProgressIndicator), findsNothing);
-  });
-
   testWidgets("Should call validate with correct values",
       (WidgetTester tester) async {
     await loadPage(tester);
