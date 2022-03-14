921697ddb0d012106a32ffcefe7182886bfb5497
rubem-pontomais <rubem.vasconcelos@pontomais.com.br>
2022-03-14 18:42:46 -0300

    feature: ensure GetxSurveysPresenter throws correct errors on failure

--- lib/presentation/presenters/getx_surveys_presenter.dart
+++ lib/presentation/presenters/getx_surveys_presenter.dart
@@ -2,7 +2,9 @@ import 'package:get/get.dart';
 import 'package:intl/intl.dart';
 import 'package:meta/meta.dart';
 
+import '../../domain/helpers/helpers.dart';
 import '../../domain/usecases/usecases.dart';
+import '../../ui/helpers/helpers.dart';
 import '../../ui/pages/surveys/surveys.dart';
 
 class GetxSurveysPresenter {
@@ -17,17 +19,21 @@ class GetxSurveysPresenter {
   GetxSurveysPresenter({@required this.loadSurveys});
 
   Future<void> loadData() async {
-    _isLoading.value = true;
-    final surveys = await loadSurveys.load();
-    _surveys.value = surveys
-        .map((survey) => SurveyViewModel(
-              id: survey.id,
-              question: survey.question,
-              date: DateFormat('dd MMM yyyy').format(survey.dateTime),
-              didAnswer: survey.didAnswer,
-            ))
-        .toList();
-
-    _isLoading.value = false;
+    try {
+      _isLoading.value = true;
+      final surveys = await loadSurveys.load();
+      _surveys.value = surveys
+          .map((survey) => SurveyViewModel(
+                id: survey.id,
+                question: survey.question,
+                date: DateFormat('dd MMM yyyy').format(survey.dateTime),
+                didAnswer: survey.didAnswer,
+              ))
+          .toList();
+    } on DomainError {
+      _surveys.subject.addError(UIError.unexpected.description);
+    } finally {
+      _isLoading.value = false;
+    }
   }
 }
--- test/presentation/presenters/getx_surveys_presenter_test.dart
+++ test/presentation/presenters/getx_surveys_presenter_test.dart
@@ -1,11 +1,13 @@
 import 'package:faker/faker.dart';
-import 'package:hear_mobile/ui/pages/surveys/survey_view_model.dart';
 import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
 
 import 'package:hear_mobile/domain/entities/entities.dart';
 import 'package:hear_mobile/domain/usecases/usecases.dart';
+import 'package:hear_mobile/domain/helpers/domain_error.dart';
 import 'package:hear_mobile/presentation/presenters/presenters.dart';
+import 'package:hear_mobile/ui/helpers/helpers.dart';
+import 'package:hear_mobile/ui/pages/pages.dart';
 
 class LoadSurveysSpy extends Mock implements LoadSurveys {}
 
@@ -29,10 +31,15 @@ void main() {
         )
       ];
 
+  PostExpectation mockLoadSurveysCall() => when(loadSurveys.load());
+
   void mockLoadSurveys(List<SurveyEntity> data) {
     surveys = data;
-    when(loadSurveys.load()).thenAnswer((_) async => surveys);
+    mockLoadSurveysCall().thenAnswer((_) async => surveys);
   }
+
+  void mockLoadSurveysError() =>
+      mockLoadSurveysCall().thenThrow(DomainError.unexpected);
 
   setUp(() {
     loadSurveys = LoadSurveysSpy();
@@ -62,6 +69,17 @@ void main() {
             didAnswer: surveys[1].didAnswer,
           )
         ])));
+
+    await sut.loadData();
+  });
+
+  test('Should throw correct errors on failure', () async {
+    mockLoadSurveysError();
+    expectLater(sut.isLoadingStream, emitsInOrder([true, false]));
+    sut.surveysStream.listen(null,
+        onError: expectAsync1(
+          (error) => expect(error, UIError.unexpected.description),
+        ));
 
     await sut.loadData();
   });
