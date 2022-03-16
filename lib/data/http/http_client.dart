7ad1b996cdfd3d9c9692e06e2dc7457bb54bef21
rubem-pontomais <rubem.vasconcelos@pontomais.com.br>
2022-03-15 21:22:52 -0300

    chore: add headers to httpClient

--- lib/data/http/http_client.dart
+++ lib/data/http/http_client.dart
@@ -2,8 +2,9 @@ import 'package:meta/meta.dart';
 
 abstract class HttpClient {
   Future<dynamic> request({
-    @required String url, 
-    @required String method, 
-    Map body
+    @required String url,
+    @required String method,
+    Map body,
+    Map headers,
   });
 }
--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -51,6 +51,20 @@ void main() {
             'accept': 'application/json'
           },
           body: '{"any_key":"any_value"}'));
+
+      await sut.request(
+          url: url,
+          method: 'post',
+          body: {'any_key': 'any_value'},
+          headers: {'any_header': 'any_value'});
+
+      verify(client.post(url,
+          headers: {
+            'content-type': 'application/json',
+            'accept': 'application/json',
+            'any_header': 'any_value'
+          },
+          body: '{"any_key":"any_value"}'));
     });
 
     test('Should call post without body', () async {
@@ -171,6 +185,18 @@ void main() {
         headers: {
           'content-type': 'application/json',
           'accept': 'application/json'
+        },
+      ));
+
+      await sut.request(
+          url: url, method: 'get', headers: {'any_header': 'any_value'});
+
+      verify(client.get(
+        url,
+        headers: {
+          'content-type': 'application/json',
+          'accept': 'application/json',
+          'any_header': 'any_value'
         },
       ));
     });
