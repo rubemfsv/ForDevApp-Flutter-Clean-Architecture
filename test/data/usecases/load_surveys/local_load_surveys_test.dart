665e3e81d84e323a0d199784615ba4ed31b55a86
rubem-pontomais <rubem.vasconcelos@pontomais.com.br>
2022-03-16 18:57:37 -0300

    feature: ensure LocalLoadSurveys calls fetch cache storage with correct key

---
+++ test/data/usecases/load_surveys/local_load_surveys_test.dart
@@ -0,0 +1,34 @@
+import 'package:meta/meta.dart';
+import 'package:mockito/mockito.dart';
+import 'package:test/test.dart';
+
+class LocalLoadSurveys {
+  final FetchCacheStorage fetchCacheStorage;
+
+  LocalLoadSurveys({@required this.fetchCacheStorage});
+
+  Future<void> load() async {
+    await fetchCacheStorage.fetch('surveys');
+  }
+}
+
+class FetchCacheStorageSpy extends Mock implements FetchCacheStorage {}
+
+abstract class FetchCacheStorage {
+  Future<void> fetch(String key);
+}
+
+void main() {
+  LocalLoadSurveys sut;
+  FetchCacheStorageSpy fetchCacheStorage;
+
+  setUp(() {
+    fetchCacheStorage = FetchCacheStorageSpy();
+    sut = LocalLoadSurveys(fetchCacheStorage: fetchCacheStorage);
+  });
+  test('Should call FetchCacheStorage with correct key', () async {
+    await sut.load();
+
+    verify(fetchCacheStorage.fetch('surveys')).called(1);
+  });
+}
