3133c69ceae4e6fb5bfe16c4058daac70b1e6239
rubem-pontomais <rubem.vasconcelos@pontomais.com.br>
2022-03-16 08:53:02 -0300

    chore: fix import bug

--- lib/main/factories/http/authorize_http_client_decorator_factory.dart
+++ lib/main/factories/http/authorize_http_client_decorator_factory.dart
@@ -1,6 +1,6 @@
 import '../../../data/http/http.dart';
 import '../../decorators/decorators.dart';
-import '../..//factories/factories.dart';
+import '../../factories/factories.dart';
 
 HttpClient makeAuthorizeHttpClientDecorator() => AuthorizeHttpClientDecorator(
       decoratee: makeHttpAdapter(),
