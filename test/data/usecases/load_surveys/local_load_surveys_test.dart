c73584f01cc3d2dc8a081a24980127253bef1b64
rubem-pontomais <rubem.vasconcelos@pontomais.com.br>
2022-03-17 20:47:01 -0300

    feature: ensure LocalLoadSurveys deletes cache if incomplete

--- test/data/usecases/load_surveys/local_load_surveys_test.dart
+++ test/data/usecases/load_surveys/local_load_surveys_test.dart
@@ -169,5 +169,18 @@ void main() {
 
       verify(cacheStorage.delete('surveys')).called(1);
     });
+
+    test('Should delete cache if it is incomplete', () async {
+      mockFetch([
+        {
+          'question': faker.randomGenerator.string(10),
+          'didAnswer': faker.randomGenerator.boolean().toString(),
+        }
+      ]);
+
+      await sut.validate();
+
+      verify(cacheStorage.delete('surveys')).called(1);
+    });
   });
 }
