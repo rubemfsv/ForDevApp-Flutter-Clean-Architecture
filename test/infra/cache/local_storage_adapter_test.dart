d3c3bb8a0a62e0e9f2660851e6115713f2b1aada
rubem-pontomais <rubem.vasconcelos@pontomais.com.br>
2022-03-17 21:33:43 -0300

    feature: ensure LocalStorageAdapter throws if deleteItem throws

--- test/infra/cache/local_storage_adapter_test.dart
+++ test/infra/cache/local_storage_adapter_test.dart
@@ -13,6 +13,9 @@ void main() {
   dynamic value;
   LocalStorageSpy localStorage;
 
+  void mockDeleteItemError() =>
+      when(localStorage.deleteItem(any)).thenThrow(Exception());
+
   setUp(() {
     localStorage = LocalStorageSpy();
     sut = LocalStorageAdapter(localStorage: localStorage);
@@ -25,5 +28,13 @@ void main() {
 
     verify(localStorage.deleteItem(key)).called(1);
     verify(localStorage.setItem(key, value)).called(1);
+  });
+
+  test('Should throw if deleteItem throws', () async {
+    mockDeleteItemError();
+
+    final future = sut.save(key: key, value: value);
+
+    expect(future, throwsA(TypeMatcher<Exception>()));
   });
 }
