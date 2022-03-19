77149be38729477ba8e20f09fcd686c30cc413bb
rubem-pontomais <rubem.vasconcelos@pontomais.com.br>
2022-03-19 16:29:14 -0300

    feature: add survey result usecase

--- lib/domain/usecases/usecases.dart
+++ lib/domain/usecases/usecases.dart
@@ -1,5 +1,6 @@
 export './add_account.dart';
 export './authentication.dart';
 export './load_current_account.dart';
+export './load_survey_result.dart';
 export './load_surveys.dart';
 export './save_current_account.dart';
