a2c73dd5ac76280803b9853a3f810f4d149c3d22
rubem-pontomais <rubem.vasconcelos@pontomais.com.br>
2022-03-20 21:17:57 -0300

    feat: add remote save survey resulte data with tests

---
+++ test/data/usecases/save_survey_result/remote_save_survey_result_test.dart
@@ -0,0 +1,133 @@
+import 'package:faker/faker.dart';
+import 'package:test/test.dart';
+import 'package:mockito/mockito.dart';
+
+import 'package:hear_mobile/domain/entities/entities.dart';
+import 'package:hear_mobile/domain/helpers/helpers.dart';
+import 'package:hear_mobile/data/http/http.dart';
+import 'package:hear_mobile/data/usecases/usecases.dart';
+
+class HttpClientSpy extends Mock implements HttpClient {}
+
+void main() {
+  RemoteSaveSurveyResult sut;
+  HttpClientSpy httpClient;
+  String url;
+  Map surveyResult;
+  String answer;
+
+  Map mockValidData() => {
+        'surveyId': faker.guid.guid(),
+        'question': faker.randomGenerator.string(50),
+        'answers': [
+          {
+            'image': faker.internet.httpUrl(),
+            'answer': faker.randomGenerator.string(20),
+            'percent': faker.randomGenerator.integer(100),
+            'count': faker.randomGenerator.integer(1000),
+            'isCurrentAccountAnswer': faker.randomGenerator.boolean()
+          },
+          {
+            'answer': faker.randomGenerator.string(20),
+            'percent': faker.randomGenerator.integer(100),
+            'count': faker.randomGenerator.integer(1000),
+            'isCurrentAccountAnswer': faker.randomGenerator.boolean()
+          },
+        ],
+        'date': faker.date.dateTime().toIso8601String(),
+      };
+
+  PostExpectation mockRequest() => when(httpClient.request(
+        url: anyNamed('url'),
+        method: anyNamed('method'),
+        body: anyNamed('body'),
+      ));
+
+  void mockHttpData(Map data) {
+    surveyResult = data;
+    mockRequest().thenAnswer((_) async => data);
+  }
+
+  void mockHttpError(HttpError error) {
+    mockRequest().thenThrow(error);
+  }
+
+  setUp(() {
+    url = faker.internet.httpUrl();
+    httpClient = HttpClientSpy();
+    sut = RemoteSaveSurveyResult(url: url, httpClient: httpClient);
+    answer = faker.lorem.sentence();
+    mockHttpData(mockValidData());
+  });
+
+  test('Should call HttpClient with correct values', () async {
+    await sut.save(answer: answer);
+
+    verify(httpClient.request(
+      url: url,
+      method: 'put',
+      body: {'answer': answer},
+    ));
+  });
+
+  test('Should return survey result on 200', () async {
+    final result = await sut.save(answer: answer);
+
+    expect(
+      result,
+      SurveyResultEntity(
+        surveyId: surveyResult['surveyId'],
+        question: surveyResult['question'],
+        answers: [
+          SurveyAnswerEntity(
+            image: surveyResult['answers'][0]['image'],
+            answer: surveyResult['answers'][0]['answer'],
+            isCurrentAnswer: surveyResult['answers'][0]
+                ['isCurrentAccountAnswer'],
+            percent: surveyResult['answers'][0]['percent'],
+          ),
+          SurveyAnswerEntity(
+            answer: surveyResult['answers'][1]['answer'],
+            isCurrentAnswer: surveyResult['answers'][1]
+                ['isCurrentAccountAnswer'],
+            percent: surveyResult['answers'][1]['percent'],
+          ),
+        ],
+      ),
+    );
+  });
+
+  test(
+      'Should throw UnexpectedError if HpptClient returns 200 with invalid data',
+      () async {
+    mockHttpData({'invalid_key': 'invalid_value'});
+
+    final future = sut.save(answer: answer);
+
+    expect(future, throwsA(DomainError.unexpected));
+  });
+
+  test('Should throw AccessDeniedError if HpptClient returns 403', () async {
+    mockHttpError(HttpError.forbidden);
+
+    final future = sut.save(answer: answer);
+
+    expect(future, throwsA(DomainError.accessDenied));
+  });
+
+  test('Should throw UnexpectedError if HpptClient returns 404', () async {
+    mockHttpError(HttpError.notFound);
+
+    final future = sut.save(answer: answer);
+
+    expect(future, throwsA(DomainError.unexpected));
+  });
+
+  test('Should throw UnexpectedError if HpptClient returns 500', () async {
+    mockHttpError(HttpError.serverError);
+
+    final future = sut.save(answer: answer);
+
+    expect(future, throwsA(DomainError.unexpected));
+  });
+}
