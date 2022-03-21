b8aa2ee49b506c7a9f9de271ceca0ab1debd2729
rubem-pontomais <rubem.vasconcelos@pontomais.com.br>
2022-03-20 21:20:54 -0300

    feat: add put infra with tests

--- lib/infra/http/http_adapter.dart
+++ lib/infra/http/http_adapter.dart
@@ -34,6 +34,10 @@ class HttpAdapter implements HttpClient {
         response = await client
             .get(url, headers: defaultHeaders)
             .timeout(Duration(seconds: 10));
+      } else if (method == 'put') {
+        response = await client
+            .put(url, headers: defaultHeaders, body: jsonBody)
+            .timeout(Duration(seconds: 10));
       }
     } catch (error) {
       throw HttpError.serverError;
